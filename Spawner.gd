84627f61cf04bb4adc8936684bec3ff4ae95d98b
Nicholas March <rival@passion.lan>
2023-12-30 20:27:12 -0800

    first attempt at spawn location around character

--- Spawner.gd
+++ Spawner.gd
@@ -2,6 +2,8 @@ extends Node
 
 var snake = preload("res://enemies/snake/Snake.tscn")
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$SnakeTimer.start()
@@ -9,5 +11,13 @@ func _ready():
 
 func _on_snake_timer_timeout():
 	var newSnake = snake.instantiate()
-	add_child(newSnake)
+	spawn_enemy_on_path(newSnake)
 
+
+func spawn_enemy_on_path(enemy: Enemy):
+	var spawn_location= get_node("../Player/SpawnPath/SpawnLocation")
+	spawn_location.progress_ratio = randf()
+	enemy.position = spawn_location.position
+
+	add_child(enemy)
+	
