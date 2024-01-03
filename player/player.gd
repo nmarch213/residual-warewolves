75b318cf58aa86e8f9a827658f267bb5b1f78a13
Troy Herbison <troy.herbison@gmail.com>
2024-01-03 16:46:12 -0600

    Add shooting mechanic that fires on movement

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@export var Bullet : PackedScene
+
+var muzzle_right_vector = Vector2(90, 0)
+var muzzle_left_vector = Vector2(-90, 0)
+var muzzle_rotation_degrees = 0
+
 	
 
 func _physics_process(_delta):
@@ -9,17 +15,31 @@ func _physics_process(_delta):
 	velocity = input_direction * speed
 	move_and_slide()
 	
-	# handle player animation based on movement
-	handle_animation(input_direction)
-
-func handle_animation(input_direction):
+	
 	if input_direction.x == 0 and input_direction.y == 0:
 		$Sprite.stop()
 	elif input_direction.x <= 0 && input_direction.y == 0:
+		$Muzzle.position = muzzle_left_vector
+		muzzle_rotation_degrees = -180
+		shoot()
 		$Sprite.play("left")
 	elif input_direction.x >= 0 && input_direction.y == 0:
+		$Muzzle.position = muzzle_right_vector
+		muzzle_rotation_degrees = 0
+		shoot()
 		$Sprite.play("right")
 	elif input_direction.y <= 0 && input_direction.x == 0:
+		shoot()
 		$Sprite.play("up")
 	elif input_direction.y >= 0 && input_direction.x == 0:
+		shoot()
 		$Sprite.play("down")
+
+		
+func shoot():
+	var b = Bullet.instantiate()
+	b.transform = $Muzzle.transform
+	b.position.y -= 20
+	b.rotation_degrees = muzzle_rotation_degrees	
+	add_child(b)
+
