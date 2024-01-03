228047fe21a8dcbb47cb5928c0333d26bf869a4f
Troy Herbison <troy.herbison@gmail.com>
2024-01-03 16:50:10 -0600

    Add timer based shooting

--- player/player.gd
+++ player/player.gd
@@ -5,9 +5,8 @@ extends CharacterBody2D
 
 var muzzle_right_vector = Vector2(90, 0)
 var muzzle_left_vector = Vector2(-90, 0)
-var muzzle_rotation_degrees = 0
+var bullet_rotation_degrees = 0
 
-	
 
 func _physics_process(_delta):
 	# handle player movement
@@ -19,27 +18,41 @@ func _physics_process(_delta):
 	if input_direction.x == 0 and input_direction.y == 0:
 		$Sprite.stop()
 	elif input_direction.x <= 0 && input_direction.y == 0:
-		$Muzzle.position = muzzle_left_vector
-		muzzle_rotation_degrees = -180
-		shoot()
 		$Sprite.play("left")
+		
+		# position muzzle on the left size of the player
+		$Muzzle.position = muzzle_left_vector
+		
+		# update this so the bullet is rotated to face forward when shot
+		bullet_rotation_degrees = -180
 	elif input_direction.x >= 0 && input_direction.y == 0:
-		$Muzzle.position = muzzle_right_vector
-		muzzle_rotation_degrees = 0
-		shoot()
 		$Sprite.play("right")
+		
+		# position muzzle on the right size of the player
+		$Muzzle.position = muzzle_right_vector
+		
+		# update this so the bullet is rotated to face forward when shot
+		bullet_rotation_degrees = 0
 	elif input_direction.y <= 0 && input_direction.x == 0:
-		shoot()
 		$Sprite.play("up")
 	elif input_direction.y >= 0 && input_direction.x == 0:
-		shoot()
 		$Sprite.play("down")
 
-		
+
+func _on_timer_timeout():
+	shoot()
+
+
 func shoot():
 	var b = Bullet.instantiate()
+	
+	# set the bullet transform to the muzzle transform (relative to the player)
 	b.transform = $Muzzle.transform
+	
+	# move the position up slightly so that the bullet comes out of the werewolve's mouth
 	b.position.y -= 20
-	b.rotation_degrees = muzzle_rotation_degrees	
-	add_child(b)
-
+	
+	# rotate the bullet so that it is facing forward when shot
+	b.rotation_degrees = bullet_rotation_degrees
+	
+	add_child(b)	
