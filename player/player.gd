4f5f2ab521ad2f63eaad492f5b77cc7d6954694e
Nicholas March <rival@passion.lan>
2024-01-06 13:02:35 -0800

    base level up

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,15 @@
 extends CharacterBody2D
 
+@export var health = 100
+@export var max_health = 100
 @export var speed = 400
 @export var Bullet : PackedScene
+
+# XP System
+@export (int) var level = 1
+var experience = 0
+var experience_total = 0
+var experience_to_level = get_required_experience_to_level(level + 1)
 
 var muzzle_right_vector = Vector2(90, 0)
 var muzzle_left_vector = Vector2(-90, 0)
@@ -54,3 +62,23 @@ func shoot():
 	b.rotation_degrees = bullet_rotation_degrees
 	
 	add_child(b)
+
+func get_required_experience_to_level(level: int):
+	return round(power(level, 1.8) + level * 4)
+
+func gain_experience(amount: int):
+	experience += amount
+	experience_total += amount
+	while experience >= experience_to_level:
+		experience -= experience_to_level
+		level_up()
+
+func level_up():
+	level += 1
+	experience_to_level = get_required_experience_to_level(level + 1)
+	print("Level Up! Level: ", level)
+	# increase the player's speed by 10%
+	speed *= 1.1
+	max_health *= 1.1
+
+
